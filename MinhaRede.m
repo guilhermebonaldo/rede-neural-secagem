cf6cae42a2b21fc4f8e4d7b8fad6d4712d23058b
Guilherme Bonaldo <guilhermebonaldo@gmail.com>
2017-11-18 19:10:24 -0200

    Update MinhaRede.m

--- MinhaRede.m
+++ MinhaRede.m
@@ -1,11 +1,11 @@
-% puxa o arquivo de excel, onde estão os dados
+% puxa o arquivo de excel, onde estÃ£o os dados
 dataset = xlsread ('DadosSecagem.xlsx','Planilha1');
 % escolhe as colunas que fazem parte do dataset
 dataset = dataset(2:end,1:5);
 
 
-% Nomeia cada coluna da database com suas respectivas variáveis
-% Normaliza as variáveis para valores entre 0 e 1
+% Nomeia cada coluna da database com suas respectivas variÃ¡veis
+% Normaliza as variÃ¡veis para valores entre 0 e 1
 
 tempo = dataset(:,1);
 t=tempo./max(tempo);
@@ -24,32 +24,32 @@ umidade = dataset (:,5);
 % Cria a transposta do dataset, para ficar no formato certo
 dataset_transp = [t,temp,peso,parte,umidade]';
 
-% ESCOLHA DO VETOR DE VALIDAÇÃO 
-% vetor [colunas a serem retiradas para validação]
+% ESCOLHA DO VETOR DE VALIDAÃ‡ÃƒO 
+% vetor [colunas a serem retiradas para validaÃ§Ã£o]
 vetor_val = [1,10,50];
 
 %------------------------------------------------------------------------
-%----------------- PARAMETROS VARIÁVEIS ---------------------------------
+%----------------- PARAMETROS VARIÃVEIS ---------------------------------
 %-------------------------------------------------------------------------
 
-HiddenSize = 5;       % num. de neuronios na camada intermediária
-meta_v = 0.997;       % Valor de R^2 desejado na validação
+HiddenSize = 5;       % num. de neuronios na camada intermediÃ¡ria
+meta_v = 0.997;       % Valor de R^2 desejado na validaÃ§Ã£o
 meta_t = meta_v;       % Valor de R^2 desejado no treino
-treino = 75/100;      % fração de separação para treino
-validacao = 15/100;    % fração de separação para validação
-teste = 10/100;        % fração de separação para teste
+treino = 75/100;      % fraÃ§Ã£o de separaÃ§Ã£o para treino
+validacao = 15/100;    % fraÃ§Ã£o de separaÃ§Ã£o para validaÃ§Ã£o
+teste = 10/100;        % fraÃ§Ã£o de separaÃ§Ã£o para teste
 epochs = 5000;         % num. maximo de epocas
-max_iteracoes = 1000;  % num. maximo de iterações
+max_iteracoes = 1000;  % num. maximo de iteraÃ§Ãµes
 erro_permitido = 1.e-12;  % erro permitido
 
 %-------------------------------------------------------------------------
 %-------------------------------------------------------------------------
 
-%retira dos dados originais as colunas para validação externa
+%retira dos dados originais as colunas para validaÃ§Ã£o externa
 dataset_t = dataset_transp;
 dataset_t = dataset_t(:,setdiff(1:size(dataset_t,2),vetor_val));
 
-%cria matriz de entradas e saidas da validação
+%cria matriz de entradas e saidas da validaÃ§Ã£o
 entradav = dataset_transp(1:end-1,vetor_val);
 saidav = dataset_transp(end,vetor_val);
 
@@ -57,7 +57,7 @@ saidav = dataset_transp(end,vetor_val);
 entradat = dataset_t(1:end-1,:);
 saidat = dataset_t(end,:);
 
-% configurações da Rede Neural 
+% configuraÃ§Ãµes da Rede Neural 
 net = newff(entradat,saidat,HiddenSize); % newff, newcf, newelm
 net.inputs{1}.processFcns = {'removeconstantrows','mapminmax'};
 net.outputs{2}.processFcns = {'removeconstantrows','mapminmax'};
@@ -72,34 +72,36 @@ net.trainParam.goal=erro_permitido;
 sinal = 1;
 i = 0;
 
-% Cria variáveis que serão utilizadas no treino
+% Cria variÃ¡veis que serÃ£o utilizadas no treino
 s=[0;0];
 maior_r=0.7;
 
-%%%%%% TREINO DA REDE 
-%enquanto o sinal for diferente de -2, faça:
+%-------------------------------------------------------------------------
+%----------------- TREINO DA REDE ----------------------------------------
+%------------------------------------------------------------------------- 
+%enquanto o sinal for diferente de -2, faÃ§a:
 while sinal~=-2 
     
    
     [net,tr] = train(net,entradat,saidat); %comando do matlab para treinar (convergir uma rede)
-    % depois de treinada, a rede estima as saidas, tanto do treino como da validação
+    % depois de treinada, a rede estima as saidas, tanto do treino como da validaÃ§Ã£o
     saidat_rede = sim(net,entradat); %Estimations
     saidav_rede = sim(net,entradav);
-    % Calcula o erro entre a real saída e a saída estimada da rede
+    % Calcula o erro entre a real saÃ­da e a saÃ­da estimada da rede
     errot = abs(saidat-saidat_rede)./saidat;
     errov = abs(saidav-saidav_rede)./saidav;
-    % Calcula o R^2 do treino e validação
+    % Calcula o R^2 do treino e validaÃ§Ã£o
     R2t = corrcoef(saidat_rede,saidat);
     R2v = corrcoef(saidav_rede,saidav);
     % Gera variavel sinal
-    % OBS.: Se R^2 for maior que o desejado, sinal será negativo
+    % OBS.: Se R^2 for maior que o desejado, sinal serÃ¡ negativo
     sinal1 = meta_v - abs(R2v(1,2));
     sinal2 = meta_t - abs(R2t(1,2));
-    % Arredonda e soma os valores da variável sinal.
+    % Arredonda e soma os valores da variÃ¡vel sinal.
     % Se os dois sinais foram negativos (desejado) para de treinar
     sinal = sum(sign([sinal1;sinal2]));
     
-    % mostra na janela de comando a iteração que está e os R^2 equivalentes
+    % mostra na janela de comando a iteraÃ§Ã£o que estÃ¡ e os R^2 equivalentes
     i
     r = [R2t(1,2), R2v(1,2)]
     
@@ -107,8 +109,8 @@ while sinal~=-2
     % ---- GRAVA OS MELHORES RESULTADOS ENCONTRADOS NO TREINO ------------
     %---------------------------------------------------------------------
     
-    % A partir da segunda iteração, se o menor dos R^2 entre o treino e
-    % validação superar o maior R^2 encontrado até então:
+    % A partir da segunda iteraÃ§Ã£o, se o menor dos R^2 entre o treino e
+    % validaÃ§Ã£o superar o maior R^2 encontrado atÃ© entÃ£o:
     
     if i > 2 && min(r) > maior_r;
        
@@ -116,14 +118,14 @@ while sinal~=-2
         maior_r = min(r);
         maiores_r = r;
      
-     % plota um grafico de correlação para o treino
+     % plota um grafico de correlaÃ§Ã£o para o treino
      figure(1)
         postreg(saidat,saidat_rede);
         xlabel('Umidade relativa experimental','FontSize',14) % nome do eixo x
         ylabel('Umidade relativa estimada','FontSize',14) % nome do eixo y
-        title( ['Saídas x Objetivos R²=' num2str(R2t(1,2))] ) % titulo do grafico
+        title( ['SaÃ­das x Objetivos RÂ²=' num2str(R2t(1,2))] ) % titulo do grafico
  
-   % Plota figuras de ajuste dos pontos retirados para validação 
+   % Plota figuras de ajuste dos pontos retirados para validaÃ§Ã£o 
     %figure(2)
     %[mv,av,rv] = postreg(saidav,saidav_rede);
     
@@ -137,7 +139,7 @@ while sinal~=-2
     % Grava a rede encontrada
     melhor_net = net;
     
-    % Pausa para poder ver os graficos ao longo das iterações
+    % Pausa para poder ver os graficos ao longo das iteraÃ§Ãµes
     pause(0.0001);
     
     end 
@@ -148,10 +150,10 @@ while sinal~=-2
     i = i+1; 
     
     %Gera uma matriz que adiciona os valores de R^2 na ultima linha apos
-    %cada iteração
+    %cada iteraÃ§Ã£o
      s = [s,r'];
             
-    % Se não convergir antes do maximo de iterações estipuladas, força a
+    % Se nÃ£o convergir antes do maximo de iteraÃ§Ãµes estipuladas, forÃ§a a
     %parada do treino
     if i== max_iteracoes
         sinal = -2;
